acf8faad0070bde1f77298724f856a741d2405b3
Darya Varabei <daria14072002@gmail.com>
2022-03-06 13:33:32 +0300

    initial commit

---
+++ io.Lavoro/io.Lavoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  io.Lavoro
+//
+//  Created by Дарья Воробей on 6.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ io.Lavoro/io.Lavoro/io_LavoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  io_LavoroApp.swift
+//  io.Lavoro
+//
+//  Created by Дарья Воробей on 6.03.22.
+//
+
+import SwiftUI
+
+@main
+struct io_LavoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
