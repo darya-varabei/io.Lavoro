c5d4cc773fd71a033939d6e17ced22c1d90d4b49
Darya Varabei <daria14072002@gmail.com>
2022-03-06 19:37:36 +0300

    add: complete log in page prototype

--- io.Lavoro/io.Lavoro/View/HomeUIView.swift
+++ io.Lavoro/io.Lavoro/View/HomeUIView.swift
@@ -18,7 +18,7 @@ struct HomeUIView: View {
             Image("logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 250)
+                .padding(.horizontal, 65)
                 .padding(.vertical)
             
             HStack {
@@ -40,14 +40,56 @@ struct HomeUIView: View {
                     .foregroundColor(.white)
                     .frame(width: 35)
                 
-                TextField("Email", text: $userName)
+                TextField("Эл.почта", text: $userName)
             }
             .padding()
             .background(Color.white.opacity(0.12))
             .cornerRadius(15)
             .padding(.horizontal)
             
+            HStack {
+                Image(systemName: "lock")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .frame(width: 35)
+                
+                SecureField("Пароль", text: $userName)
+            }
+            .padding()
+            .background(Color.white.opacity(0.12))
+            .cornerRadius(15)
+            .padding(.horizontal)
+            .padding(.top)
+            
+            Button(action: {}, label: {
+                Text("Войти")
+                    .fontWeight(.heavy)
+                    .font(.custom("", size: 14))
+                    .foregroundColor(.black)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 150)
+                    .background(Color.blue)
+                    .clipShape(Capsule())
+            })
+                .padding(.top)
+            
+            Button(action: {}, label: {
+                Text("Forgot passsword?")
+                    .foregroundColor(Color.green)
+            }).padding(.top, 8)
+            
             Spacer(minLength: 0)
+            
+            HStack(spacing: 5) {
+                Text("Don't have an account?")
+                    .foregroundColor(Color.white.opacity(0.6))
+                
+                Button(action: {}, label: {
+                    Text("Sign up")
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.blue)
+                })
+            }.padding(.vertical)
         }
         
         .background(Color.green.ignoresSafeArea(.all, edges: .all))
